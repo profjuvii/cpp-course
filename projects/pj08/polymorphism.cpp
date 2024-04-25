5970b3d3091400c0dbfd71df206150201fc19bfb
Denys Bondarchuk <profjuvi@gmail.com>
2024-04-25 16:09:57 +0200

    add polymorphism.cpp

---
+++ projects/pj08/polymorphism.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+
+// base class
+class Shape{
+public:
+    // virtual method
+    virtual void draw() const {
+        cout << "Drawing a shape." << endl;
+    }
+};
+
+
+// derived class Circle
+class Circle : public Shape{
+public:
+    // Override the virtual function from the base class
+    void draw() const override {
+        cout << "Drawing a circle." << endl;
+    }
+};
+
+
+// derived class Square
+class Square final : public Shape {
+public:
+    // Override the virtual function from the base class
+    void draw() const override {
+        cout << "Drawing a square" << endl;
+    }
+};
+
+
+void drawShape(const Shape& shape) {
+    shape.draw(); // polymorphic call
+}
+
+
+int main(int argc, char** argv){
+    Circle circle;
+    drawShape(circle);
+
+    Square square;
+    drawShape(square);
+
+    return 0;
+}
